296eb2b8ebb32a4c96d12ab03a83d2e6d589415a
Jamie Phillips <cdrock7@gmail.com>
2024-12-01 21:19:53 -0600

    fix day 1

---
+++ day-2/a.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./input-a.txt', 'UTF-8');
+const lines = data.split(/\r?\n/);
+
+let sum = 0;
+
+lines.forEach(line => {
+
+});
+
+console.log(sum);
