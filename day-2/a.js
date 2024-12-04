2647ee6db0e617273ac4968e0eacc7f40977ab68
Jamie Phillips <cdrock7@gmail.com>
2024-12-03 21:38:45 -0600

    day 2

--- day-2/a.js
+++ day-2/a.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./input-a.txt', 'UTF-8');
+// const data = fs.readFileSync('./test.txt', 'UTF-8');
+
 const lines = data.split(/\r?\n/);
 
 let sum = 0;
 
 lines.forEach(line => {
+  const a = line.split(' ').map((n) => parseInt(n));
 
+  let good = true;
+  let increasing = a[0] < a[1];
+
+  for (let i = 0; i < a.length - 1; i++) {
+    if ((increasing && a[i] > a[i+1] || !increasing && a[i] < a[i+1]) || Math.abs(a[i] - a[i+1]) > 3 || Math.abs(a[i] - a[i+1]) < 1) {
+      good = false;
+      break;
+    }
+  }
+
+  if (good) sum++;
 });
 
 console.log(sum);
--- day-2/b.js
+++ day-2/b.js
@@ -1,12 +1,40 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./input-b.txt', 'UTF-8');
+// const data = fs.readFileSync('./test.txt', 'UTF-8');
+
 const lines = data.split(/\r?\n/);
 
 let sum = 0;
 
 lines.forEach(line => {
+  const data = line.split(' ').map((n) => parseInt(n));
 
+  let checkArray = (a, stop) => {
+    let checkIfBad = (x, y) => {
+      const bad = increasing && x > y || !increasing && x < y || Math.abs(x - y) > 3 || Math.abs(x - y) < 1;
+      return bad;
+    }
+
+    let increasing = a[0] < a[1];
+
+    for (let i = 0; i < a.length - 1; i++) {
+      if (checkIfBad(a[i], a[i+1])) {
+        if (stop) {
+          return false;
+        }
+  
+        const ar2 = a.filter((_, index) => index !== i);
+        const ar3 = a.filter((_, index) => index !== i+1);
+        const ar4 = a.filter((_, index) => index !== i-1);
+        return checkArray(ar2, true) || checkArray(ar3, true) || checkArray(ar4, true);
+      }
+    }
+
+    return true;
+  }
+
+  if(checkArray(data, false)) sum++;
 });
 
 console.log(sum);
